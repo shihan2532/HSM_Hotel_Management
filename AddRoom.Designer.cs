b849adfebc0cfd1bda31adf56c29b1e0722ad4a2
farhan shadik shihan- pc <farhanshihan2002@gmail.com>
2026-05-18 03:17:56 +0600

    picturebox deleted

--- AddRoom.Designer.cs
+++ AddRoom.Designer.cs
@@ -270,7 +270,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+          
             this.pictureBox1.Location = new System.Drawing.Point(6, 25);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(57, 63);
